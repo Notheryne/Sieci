e57b3da6f173599fd5dd816420da351a86a7fce4
Notheryne <rydzinski.bartosz.1998@gmail.com>
2019-12-09 20:55:05 +0100

    Kruskal and Prim beggining

--- lab3/main.py
+++ lab3/main.py
@@ -17,6 +17,12 @@ class Edge:
     def w(self):
         return self._weight
 
+    def __eq__(self, other):
+        return self.s == other.s and self.d == self.d and self.w == self.w
+
+    def contains_two_trees(self):
+        return not self._source == self._destination
+
     def describe(self):
         return '[%s] From %s to %s (w: %s)' % (self._name, self._source, self._destination, self._weight)
 
@@ -25,16 +31,27 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, vertices, data=None, filepath=None, skip_header=False):
+    def __init__(self, vertices=None, data=None, filepath=None, skip_header=False):
         self.data = []
         if filepath:
             self.get_from_file(filepath, skip_header=skip_header)
         elif data:
-            self.data = [Edge(i, weight=data[0], source=data[1], destination=data[2])
+            self.data = [Edge('I am ' + str(i), weight=data[0], source=data[1], destination=data[2])
                          for i in range(len(data))]
         else:
             raise ValueError('No initialization data provided!')
-        self.vertices = [i+1 for i in range(vertices)]
+        if vertices:
+            self.vertices = vertices
+        else:
+            self.vertices = set()
+            for edge in self.data:
+                self.vertices.add(edge.s)
+                self.vertices.add(edge.d)
+                self.vertices = list(self.vertices)
+        self.parents = {vertex: vertex for vertex in self.vertices}
+        self.ranks = {vertex: 0 for vertex in self.vertices}
+        self.max_weight = max(self.data, key=lambda x: x.w).w
+        self.mst = []
 
     @property
     def v(self):
@@ -46,14 +63,48 @@ class Graph:
         if skip_header:
             data = data[1:]
         print(data)
-        self.data = [Edge(i, weight=int(data[i][0]), source=int(data[i][1]),
+        self.data = [Edge('I am ' + str(i), weight=int(data[i][0]), source=int(data[i][1]),
                           destination=int(data[i][2])) for i in range(len(data))]
+
+    def kruskal_find(self, vertex):
+        if self.parents[vertex] != vertex:
+            self.parents[vertex] = self.kruskal_find(self.parents[vertex])
+        return self.parents[vertex]
+
+    def kruskal_union(self, source, destination):
+        source_root = self.kruskal_find(source)
+        dest_root = self.kruskal_find(destination)
+        if source_root != dest_root:
+            if self.ranks[source_root] > self.ranks[dest_root]:
+                self.parents[dest_root] = source_root
+            else:
+                self.parents[source_root] = dest_root
+            if self.ranks[source_root] == self.ranks[dest_root]:
+                self.ranks[dest_root] += 1
 
     def kruskal(self):
         self.data.sort(key=lambda x: x.w)
+        for edge in self.data:
+            if self.kruskal_find(edge.s) != self.kruskal_find(edge.d):
+                self.kruskal_union(edge.s, edge.d)
+                self.mst.append(edge)
+        self.mst.sort(key=lambda x: x.w)
+        print(self.mst)
 
+    def prim(self):
+        costs = [self.max_weight + 1] * len(self.vertices)
+        edges = [None] * len(self.vertices)
+        result_forest = []
+        not_needed = []
+        for i in range(len(self.vertices)):
+            appearances = [edge for edge in self.data
+                           if edge.d == self.vertices[i]]
+            best_match = min(appearances, key=lambda x: x.w)
+            result_forest.append(best_match)
+            if not edges[i]:
+                edges[i] =
 
-g = Graph(4, filepath='trees/1.txt')
-print(g.data)
-g.kruskal()
-print(g.data)
+g = Graph(filepath='trees/2.txt')
+# print(g.data)
+# g.kruskal()
+# print(g.data)
